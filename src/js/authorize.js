b5bbd44f5dddb4c0f2741aaa5ca228135bf89e47
Birk <birk.kensen@gmail.com>
2022-02-17 12:18:30 +0100

    changed redirect uri

--- src/js/authorize.js
+++ src/js/authorize.js
@@ -1,7 +1,8 @@
 const loginBtn = document.getElementById('login-button');
 loginBtn.addEventListener('click', () => {
   const client_id = process.env.CLIENT_ID;
-  const redirect_uri = 'http://localhost:8080/playlist.html';
+  const redirect_uri =
+    'https://playlist-visualisation.netlify.app/playlist.html';
 
   let url = 'https://accounts.spotify.com/authorize';
   url += '?response_type=token';
